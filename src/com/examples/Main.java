440f20a4c380135693421200f8c16bb8b8efc284
zhangdaihui <spartazhangzhan@gmail.com>
2021-02-20 11:26:05 +0800

    init

---
+++ src/com/examples/Main.java
@@ -0,0 +1,13 @@
+package com.examples;
+import com.examples.utils.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // start node
+        System.out.println("this is a nice start");
+        // simple one
+        simple_1.print();
+        //
+    }
+}
---
+++ src/com/examples/utils/simple_1.java
@@ -0,0 +1,7 @@
+package com.examples.utils;
+
+public class simple_1 {
+    public static void print(){
+        System.out.printf("%d",123);
+    }
+}
