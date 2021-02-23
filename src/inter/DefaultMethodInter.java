f7f1d1db766d815ea9c88de51970ce913e6475f0
zhangdaihui <spartazhangzhan@gmail.com>
2021-02-23 15:34:20 +0800

    接口默认方法冲突的问题

--- src/inter/DefaultMethodInter.java
+++ src/inter/DefaultMethodInter.java
@@ -6,10 +6,22 @@ interface DI{
     }
 }
 
-public class DefaultMethodInter implements DI{
-    public String getStr(){
-        return "override interface default method";
+interface DI2{
+    default String getStr(){
+        return "interface default method2";
     }
+}
+
+interface DIMixin extends DI,DI2{
+    // 必须重写默认方法
+    @Override
+    default String getStr() {
+        return DI2.super.getStr();
+    }
+}
+
+public class DefaultMethodInter implements DIMixin{
+
 
     public void print(){
         String code =  getStr();
