65dd6c171142e9f669f8881e538da74291f394cb
zhangdaihui <spartazhangzhan@gmail.com>
2021-02-24 11:08:33 +0800

    多个异常处理

--- src/exception/TryCatch.java
+++ src/exception/TryCatch.java
@@ -4,9 +4,9 @@ public class TryCatch {
     public static void print(){
         try {
             int i=0;
-            double j = 1/i;
-
-        }catch (ArithmeticException e){
+//            double j = 1/i;
+            throw new IllegalAccessException("测试异常");
+        }catch (ArithmeticException|IllegalAccessException e){
             System.out.println("exception "+e);
         }finally {
             System.out.println("finally done!");
